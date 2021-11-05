4dbdc15c0df3d9dcbe45ed8d417b3f590371e6ec
ilenzio <westfury@gmail.com>
2021-11-05 17:31:52 -0400

    exer: 'grade-school' solved

--- exercism/grade-school/src/main/scala/GradeSchool.scala
+++ exercism/grade-school/src/main/scala/GradeSchool.scala
@@ -5,8 +5,6 @@ case class School (var db: Map[Int, Seq[String]] = Map())  {
 
 
   def add(name: String, g: Int) = {
-//    println("nothing")
-
     val temp =  this.db.getOrElse(g, Seq()) :+ name
     db = db + (g -> temp)
 
@@ -19,9 +17,17 @@ case class School (var db: Map[Int, Seq[String]] = Map())  {
   }
 
   def sorted: DB = {
-
-    val temp = ListMap(this.db.toSeq.sortBy(_._1):_*)
-    temp
+    db.keys.foreach(i => db = db + (i -> db(i).sorted))
+    val finalDB = ListMap(this.db.toSeq.sortBy(_._1):_*)
+    finalDB
   }
 }
-
+// another solution
+//class School {
+//  type DB = Map[Int, Seq[String]]
+//  private var _db: DB = Map.empty
+//  def add(name: String, g: Int) = _db = _db.updated(g, grade(g) :+ name)
+//  def db: DB = _db
+//  def grade(g: Int): Seq[String] = _db.getOrElse(g, Seq.empty)
+//  def sorted: DB = Map(_db.toSeq.sortBy(_._1): _*).mapValues(_.sorted)
+//}
